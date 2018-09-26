6950206969309fe39131448faad21ba3869e5fba
sahincabbar <36867588+sahincabbar@users.noreply.github.com>
2018-09-26 19:02:49 +0400

    Database Added

---
+++ 23092018ef-databasefirst-shahinjabbar-master/DatabaseFirst/Form1.cs
@@ -0,0 +1,49 @@
+﻿using Credits.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Credits
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btn_takecredit_Click(object sender, EventArgs e)
+        {
+            using (BankDbEntities bn = new BankDbEntities())
+            {
+                try
+                {
+                    if (txbx_name.Text != null && txbx_surname.Text != null && txbx_passportnumber.Text != null)
+                    {
+                        bn.usp_AddCustomers(txbx_name.Text, txbx_surname.Text, Convert.ToInt32(txbx_passportnumber.Text));
+                        TakeCreditForm tcf = new TakeCreditForm();
+                        tcf.ShowDialog();
+                    }
+                }
+                catch (FormatException exp)
+                {
+
+                    throw exp;
+                }
+
+            }
+           
+        }
+    }
+}
---
+++ 23092018ef-databasefirst-shahinjabbar-master/DatabaseFirst/Forms/TakeCreditForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Credits.Models;
+
+namespace Credits.Forms
+{
+    public partial class TakeCreditForm : Form
+    {
+        public TakeCreditForm()
+        {
+            InitializeComponent();
+        }
+        private void btb_addcredit_Click(object sender, EventArgs e)
+        {
+            using (BankDbEntities bn = new BankDbEntities())
+            {
+                try
+                {
+                    
+                    if (comboBox1.Text != "" && textBox1.Text != null)
+                    {
+                        bn.usp_Givecredit(comboBox1.SelectedItem.ToString(), Convert.ToInt32(textBox1.Text), dateTimePicker1.Value, bn.Customers.ToList().Last().Id);
+                        ShowAllForm showAllForm = new ShowAllForm();
+                        showAllForm.ShowDialog();
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        private void TakeCreditForm_Load(object sender, EventArgs e)
+        {
+            using (BankDbEntities bn = new BankDbEntities())
+            {
+                List<Customer> customers = bn.Customers.Where(x => x.Id > 0).ToList();
+                comboBox1.Items.Add(bn.Customers.ToList().Last().Name);
+            }
+        }
+
+    }
+}
